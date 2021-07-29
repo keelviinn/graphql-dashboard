687f6eed537ae237707926880fc3770c8286587f
Kelvin Oliveira <keelviinn@gmail.com>
2021-07-28 21:45:38 -0300

    header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export default function Profile() {
+interface ProfileProps {
+  showProfileData: boolean 
+}
+
+export default function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Kelvin Oliveira</Text>
-        <Text>keelviinn@gmail.com</Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Kelvin Oliveira</Text>
+          <Text>keelviinn@gmail.com</Text>
+        </Box>
+      ) }      
 
       <Avatar size="md" name="Kelvin Oliveira" src="https://avatars.githubusercontent.com/u/50298432?v=4" />
     </Flex>
