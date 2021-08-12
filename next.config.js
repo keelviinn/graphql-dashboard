49090c131ceb8724262cad99eeabcbe8ffef7f21
Kelvin Oliveira <keelviinn@gmail.com>
2021-08-12 09:08:05 -0300

    env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    NEXT_APP_API_URL: 'http://localhost:8080' ,
-    NEXT_WS_API_URL: 'ws://localhost:8080'
+    NEXT_APP_API_URL: NEXT_APP_API_URL === 'DEV' ? 'http://localhost:8080' : 'https://graphql-api-products.herokuapp.com/' ,
+    NEXT_WS_API_URL: NEXT_APP_API_URL === 'DEV' ? 'ws://localhost:8080' : 'wss://graphql-api-products.herokuapp.com/'
   },
 }
